059bf9f7cc6e4952f8add3eb04934af7c98753ba
Stephanie <alu0101351728@ull.edu.es>
2022-04-24 04:12:40 +0000

    fix shell

--- main.d
+++
@@ -1,4 +0,0 @@
-bin/main.o: src/main.cpp src/./include/selection.hpp \
- src/./include/ordenacion.hpp src/./include/radix.hpp \
- src/./include/shell.hpp src/./include/heap.hpp src/./include/quick.hpp \
- src/./include/ordenacion.hpp
